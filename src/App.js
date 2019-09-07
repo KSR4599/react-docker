891d48635f65e7d0c25576b6fa224730a81608b5
KSR <ksreddy9945@gmail.com>
2019-09-07 17:49:17 +0530

    testing travis7

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          DOCKER + KUBERNETES = WOW!
+          WASSUP WASSUP MY DOCKER CHEEZZEEE
         </p>
         <a
           className="App-link"
